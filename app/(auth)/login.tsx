156b2c5974616a5b5629877b0b1a2cf01f0a5a64
steinerhippo <steinerhippo@platinumzone24.at>
2023-04-20 17:35:41 +0200

    fix two factor login flow

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -212,6 +212,8 @@ export default function LoginScreen() {
           });
           return;
         }
+
+        login(data.token, data.refresh_token);
       })
       .catch((error) => {
         notification.showNotification(
